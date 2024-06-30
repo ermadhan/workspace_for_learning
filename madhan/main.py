48df826e0b71137230354780e97db0f298928583
ermadhan <ermadhan@gmail.com>
2024-06-30 19:53:23 +0530

    simple program

--- madhan/main.py
+++ madhan/main.py
@@ -1,1 +1,5 @@
-import 
+import osmnx as ox
+
+ox.config(use_cache=True, log_console=True)
+gdf = ox.geometries_from_place('Piedmont, CA, USA', tags={'building':True})
+fig, ax = ox.plot_footprints(gdf)
