c07a125ba840f2f54f3a8bd77b8f6535d401b948
Tharanitharan388 <tharani9043@gmail.com>
2024-06-30 08:50:12 -0600

    Update main.py

--- madhan/main.py
+++ madhan/main.py
@@ -1,5 +1,1 @@
-import osmnx as ox
-#WEEK 2 of GITHUB
-ox.config(use_cache=True, log_console=True)
-gdf = ox.geometries_from_place('Piedmont, CA, USA', tags={'building':True})
-fig, ax = ox.plot_footprints(gdf)
+print("hello")
